31eff5b76ed2f1ca6eacc87c15c54292a5efe5f2
Indika <indeka9@yahoo.com>
2025-07-19 20:13:58 +0530

    Update btrfs.sh

--- btrfs.sh
+++ btrfs.sh
@@ -63,6 +63,9 @@ done
 
 
 echo "rsync -aAXv --numeric-ids --delete ${RSYNC_FROM_DIR}/ ${RSYNC_TO_DIR}/"
+echo "pacstrap ${RSYNC_TO_DIR} base, linux, linux-headers linux-firmware"
+
+echo "genfstab -U -P ${RSYNC_TO_DIR} >> ${RSYNC_TO_DIR}/etc/fstab" 
 
 echo "mount --types proc /proc /mnt/copyto/proc"
 echo "mount --rbind /sys /mnt/copyto/sys"
@@ -76,4 +79,10 @@ echo "chmod 1777 /mnt/copyto/dev/shm"
 echo "chroot /mnt/copyto /bin/bash"
 echo "source /etc/profile"
 echo "export PS1=\"(chroot) \$PS1\""
+
+modules='all_video boot btrfs cat chain configfile echo efifwsetup efinet ext2 fat font gettext gfxmenu gfxterm gfxterm_background gzio halt help hfsplus iso9660 jpeg keystatus loadenv loopback linux ls lsefi lsefimmap lsefisystab lssal memdisk minicmd normal ntfs part_apple part_msdos part_gpt password_pbkdf2 png probe reboot regexp search search_fs_uuid search_fs_file search_label sleep smbios squash4 test true video xfs zfs zfscrypt zfsinfo play tpm luks cryptodisk cpuid'
+echo "grub-install  --bootloader-id Linux --efi-directory /boot  --modules=\"${modules}\" --sbat /usr/share/grub/sbat.csv"
+echo "efibootmgr --create --disk /dev/${DISK} --part ${DISK}${DISKEFI} --label "Linux shim" --loader 'EFI\BOOT\shimx64.efi' --unicode"
+
+
 
